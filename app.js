6a9ab6ec20605eeb5131a80ecd139e8b3f2512b6
Sandeep Soni <sandeepsonisrs01@gmail.com>
2022-10-16 17:41:31 +0530

    added option in cors

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 3000;
 app.use(cors(
     {
         origin: ["https://noteforyouapp.herokuapp.com", "http://localhost:4200"],
-        methods: ["GET", "POST", "PUT"]
+        methods: ["GET", "POST", "PUT", "OPTIONS"]
     }
     ));
 app.use(bodyParser.json());
